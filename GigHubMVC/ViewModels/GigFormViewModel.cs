389745697e9d4c07c89183c05acc84c7c90c7715
Panos Bozas <fbozas@hotmail.com>
2022-06-14 20:50:58 +0300

    Created "Add a Gig" Form

---
+++ GigHubMVC/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GigHubMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHubMVC.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Genre { get; set; }
+        public ICollection<Genre> Genres { get; set; }
+
+    }
+}
