ebd5d175b9fc2f16e200702d5ecc5669df94947f
Panos Bozas <fbozas@hotmail.com>
2022-06-15 19:03:32 +0300

    Added and use foreign key properties on Gig instead of navigation properties

--- GigHubMVC/Models/Gig.cs
+++ GigHubMVC/Models/Gig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,8 +11,11 @@ namespace GigHubMVC.Models
     {
         public int Id { get; set; }
 
-        [Required]
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        [ForeignKey("Artist")]
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
@@ -19,7 +23,10 @@ namespace GigHubMVC.Models
         [StringLength(255)]
         public string Venue { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
+        [ForeignKey("Genre")]
+        public byte GenreId { get; set; }
     }
 }
