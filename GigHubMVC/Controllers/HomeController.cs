7dd99ef4fe8964f6c9f6aa37f8f9ca3bc3839cbe
Panos Bozas <fbozas@hotmail.com>
2022-06-16 21:39:14 +0300

    Add Name to Artist

--- GigHubMVC/Controllers/HomeController.cs
+++ GigHubMVC/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using GigHubMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace GigHubMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Where(g => g.DateTime > DateTime.Now);
+
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
