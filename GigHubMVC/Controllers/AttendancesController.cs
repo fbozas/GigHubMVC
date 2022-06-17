bb08408e0fb56438a472fb068dd24e59d76a854b
Panos Bozas <fbozas@hotmail.com>
2022-06-17 19:40:56 +0300

    Added our first Web Api Controller

---
+++ GigHubMVC/Controllers/AttendancesController.cs
@@ -0,0 +1,37 @@
+﻿using GigHubMVC.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GigHubMVC.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int gigId)
+        {
+            var attendance = new Attendance()
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
