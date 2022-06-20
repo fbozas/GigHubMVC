950965693d1f84d7bb550981f119d8c7f3c051c7
Panos Bozas <fbozas@hotmail.com>
2022-06-20 21:02:26 +0300

    Added Following feature

--- GigHubMVC/Controllers/FollowingsController.cs
+++ GigHubMVC/Controllers/FollowingsController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GigHubMVC.Dtos;
+using GigHubMVC.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,37 @@ using System.Web.Http;
 
 namespace GigHubMVC.Controllers
 {
+    [Authorize]
     public class FollowingsController : ApiController
     {
+        private readonly ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FollowerId == userId && f.FollowerId == dto.FolloweeId))
+                return BadRequest("Following already exists");
+
+            //if (userId == dto.FolloweeId)
+            //    return BadRequest("The Artist cannot follow himself");
+
+            var following = new Following()
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
