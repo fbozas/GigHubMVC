65d2f6a34efa9264f97f89ec2d0ddc6be40a70da
Panos Bozas <fbozas@hotmail.com>
2022-06-24 20:50:02 +0300

    Implemented my Upcoming Gigs Page

--- GigHubMVC/Controllers/GigsController.cs
+++ GigHubMVC/Controllers/GigsController.cs
@@ -20,6 +20,18 @@ namespace GigHubMVC.Controllers
         }
 
         [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+
+            return View(gigs);
+        }
+
+        [Authorize]
         public ActionResult Attending()
         {
             var userId = User.Identity.GetUserId();
@@ -73,7 +85,7 @@ namespace GigHubMVC.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
