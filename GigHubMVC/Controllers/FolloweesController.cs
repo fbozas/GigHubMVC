6b7dd0faa30f79eb5d91adf90ce2cc858b0bbb29
Panos Bozas <fbozas@hotmail.com>
2022-06-24 20:24:18 +0300

    Implemented "Artists I 'm Following" feature

---
+++ GigHubMVC/Controllers/FolloweesController.cs
@@ -0,0 +1,38 @@
+﻿using GigHubMVC.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GigHubMVC.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Followees
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            // If I wanted the followers of  loggedin user.....
+            //var followers = _context.Followings
+            //    .Where(f => f.FolloweeId == userId)
+            //    .Select(f => f.Follower)
+            //    .ToList();
+
+            return View(artists);
+        }
+    }
+}
